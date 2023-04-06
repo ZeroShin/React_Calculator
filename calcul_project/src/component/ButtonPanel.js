61789ea330d2868c3061377752b5edf9297be8aa
young <yhs5826@gmail.com>
2023-04-06 22:35:56 +0900

    ui_add

---
+++ calcul_project/src/component/ButtonPanel.js
@@ -0,0 +1,51 @@
+import Button from "./Button";
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./ButtonPanel.css";
+
+export default class ButtonPanel extends React.Component {
+  static propTypes = {
+    clickHandler: PropTypes.func,
+  };
+
+  handleClick = buttonName => {
+    this.props.clickHandler(buttonName);
+  };
+
+  render() {
+    return (
+      <div className="component-button-panel">
+        <div>
+          <Button name="AC"  />
+          <Button name="+/-"  />
+          <Button name="%"  />
+          <Button name="÷"  orange />
+        </div>
+        <div>
+          <Button name="7"  />
+          <Button name="8"  />
+          <Button name="9"  />
+          <Button name="x"  orange />
+        </div>
+        <div>
+          <Button name="4"  />
+          <Button name="5"  />
+          <Button name="6"  />
+          <Button name="-"  orange />
+        </div>
+        <div>
+          <Button name="1"  />
+          <Button name="2"  />
+          <Button name="3"  />
+          <Button name="+"  orange />
+        </div>
+        <div>
+          <Button name="0"  wide />
+          <Button name="."  />
+          <Button name="="  orange />
+        </div>
+      </div>
+    );
+  }
+}
